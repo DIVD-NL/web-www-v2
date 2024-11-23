efdddf4a0609a61d273c644c3f240f09557e9032
Marten de Groot <marten.de.groot@divd.nl>
2024-11-23 16:36:24 +0100

    Added DIVD colors to TS config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,16 @@ module.exports = {
     screens: {
       desktop: '1280px',
     },
+    extend: {
+      colors: {
+        'light-black': '#1D1D1D',
+        'dark-gray': '#545454',
+        gray: '#D1D1D1',
+        'light-gray': '#F2F2F2',
+        'DIVD-yellow': '#FFD736',
+        'DIVD-turquoise': '#3DDCEB',
+      },
+    },
   },
   plugins: [],
 };
