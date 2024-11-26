f630a56aabcfb0fcdb478e82314a5770c1565d7b
Marten de Groot <marten.de.groot@divd.nl>
2024-11-26 07:17:27 +0100

    Added font definitions

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,18 @@ module.exports = {
         'DIVD-yellow': '#FFD736',
         'DIVD-turquoise': '#3DDCEB',
       },
+      fontSize: {
+        'heading-1': ['5rem', { lineHeight: '5.25rem', letterSpacing: '0.1rem', fontWeight: '900' }],
+        'heading-2': ['3.5rem', { lineHeight: '4.125rem', letterSpacing: '0.07rem', fontWeight: '900' }],
+        'heading-3': ['2rem', { lineHeight: '5.25rem', letterSpacing: '0.04rem', fontWeight: '700' }],
+        'heading-4': ['1.55rem', { lineHeight: '5.25rem', letterSpacing: '0.031rem', fontWeight: '700' }],
+        'subheading-1': ['2rem', { lineHeight: '2.75rem', letterSpacing: '0.04rem', fontWeight: '400' }],
+        body: ['1.25rem', { lineHeight: '2rem', letterSpacing: '0.025rem', fontWeight: '400' }],
+      },
+      fontFamily: {
+        heading: ['Source Sans 3, sans-serif'],
+        body: ['Open Sans, sans-serif'],
+      },
     },
   },
   plugins: [],
