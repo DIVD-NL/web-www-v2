fffd70980090fe31aa13ebff76f28cb55f9b3c4d
MagicLegend <MagicLegend@users.noreply.github.com>
2024-12-03 14:37:14 +0100

    + Vite CMS build and auto-deploy to Github Pages side by side with the old CMS

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   root: 'cms', // Set the root to `cms` since `index.html` is there
   build: {
-    outDir: '../dist', // Output directory for the build (relative to root)
+    outDir: '../admin/new-cms', // Output directory for the build (relative to root)
     emptyOutDir: true, // Clear output directory before building
+    sourcemap: true, // Enable sourcemaps
   },
   resolve: {
     alias: {
