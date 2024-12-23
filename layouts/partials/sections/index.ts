24a494ddc69113f390acaf0260541c5dd14efa7b
Marten de Groot <marten.de.groot@divd.nl>
2024-12-23 22:08:41 +0100

    Cleanup Why Our Work Matters experiment

--- layouts/partials/sections/index.ts
+++ layouts/partials/sections/index.ts
@@ -1,9 +1,3 @@
-import { DoubleDecker } from './double-decker';
 import { Faq } from './faq';
-import { Paper } from './paper';
-import { Pill } from './pill';
-import { SuggestedArticles } from './suggested-articles';
-import { TitleText } from './title-text';
 
-export const sections = [DoubleDecker, Paper, Pill, SuggestedArticles, TitleText, Faq];
-export const articleSections = [SuggestedArticles, Faq];
+export const articleSections = [Faq];
