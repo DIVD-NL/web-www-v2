a913a3258e12235225b7261192fd7ae6f8cf03d1
Marten de Groot <marten.de.groot@divd.nl>
2024-12-27 19:11:40 +0100

    Home collection

--- cms/collections.ts
+++ cms/collections.ts
@@ -1,4 +1,5 @@
+import { HomeCollection } from '../layouts/collection';
 import { DynamicFaqCollection, FaqCollection } from '../layouts/faq/collection';
 import { Collection as ArticlesCollection } from '../layouts/newsroom/collection';
 
-export default [ArticlesCollection, DynamicFaqCollection, FaqCollection];
+export default [HomeCollection, ArticlesCollection, DynamicFaqCollection, FaqCollection];
