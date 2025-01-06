c961605a7e8afa114b060fdcf65879d52429fb0c
Marten de Groot <marten.de.groot@divd.nl>
2025-01-06 19:46:22 +0100

    Revert "Added FAQ Collection"
    This reverts commit 271bab61936b6fb3cfc731a621039580bc8d62f3.

--- cms/collections.ts
+++ cms/collections.ts
@@ -1,4 +1,3 @@
-import { DynamicFaqCollection, FaqCollection } from '../layouts/faq/collection';
 import { Collection as ArticlesCollection } from '../layouts/newsroom/collection';
 
-export default [ArticlesCollection, DynamicFaqCollection, FaqCollection];
+export default [ArticlesCollection];
