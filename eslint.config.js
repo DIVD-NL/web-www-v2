768a3f8e477adfd7481b5d9fa846c54a896a0a48
Marten de Groot <marten.de.groot@divd.nl>
2025-02-04 19:10:27 +0100

    Migrate ESLint and update config

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+// Import the necessary modules
+import js from '@eslint/js';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      sourceType: 'module',
+      ecmaVersion: 'latest',
+    },
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...prettierPlugin.configs.recommended.rules,
+    },
+  },
+  // Prettier's configuration to disable conflicting ESLint rules
+  prettierConfig,
+  {
+    // Note: there should be no other properties in this object
+    ignores: ['node_modules/', 'dist/', 'public/', 'static/'],
+  },
+];
