c3b5fcef689e9dee5bbc19c3f4f5c3f711e37ee1
Marten de Groot <marten.de.groot@divd.nl>
2025-02-11 19:06:58 +0100

    Added article cards section

--- layouts/partials/sections/index.ts
+++ layouts/partials/sections/index.ts
@@ -1,9 +1,16 @@
+import { ArticleCardsSection } from './article-cards';
 import { Faq } from './faq';
 import { HeroSection } from './hero';
-import { HighlightedCases } from './highlighted-cases';
+import { HighlightedCasesSection } from './highlighted-cases';
 import { PartnerSection } from './partner';
 import { TwoColumnsSection } from './two-columns';
 
-export const landingPageSections = [HeroSection, HighlightedCases, PartnerSection, TwoColumnsSection];
+export const landingPageSections = [
+  ArticleCardsSection,
+  HeroSection,
+  HighlightedCasesSection,
+  PartnerSection,
+  TwoColumnsSection,
+];
 
 export const articleSections = [Faq];
