b7c6f33344b71106d9b05fe71507e7fac53c50bb
Sk Imtiaz Ahmed <imtiaz101325@gmail.com>
2025-02-12 19:19:10 +0100

    [725] feat: Adds slider section with configuration and template

---
+++ layouts/partials/sections/slider/index.ts
@@ -0,0 +1,40 @@
+export const SliderSection = {
+  label: 'Slider Section',
+  required: false,
+  name: 'slider-section',
+  widget: 'object',
+  fields: [
+    {
+      label: 'Section title',
+      name: 'title',
+      widget: 'string',
+    },
+    {
+      label: 'Section description',
+      name: 'description',
+      widget: 'text',
+    },
+    {
+      label: 'Cards',
+      name: 'cards',
+      widget: 'list',
+      fields: [
+        {
+          label: 'Card title',
+          name: 'title',
+          widget: 'string',
+        },
+        {
+          label: 'Card description',
+          name: 'description',
+          widget: 'text',
+        },
+        {
+          label: 'Card image',
+          name: 'image',
+          widget: 'image',
+        },
+      ],
+    },
+  ],
+};
