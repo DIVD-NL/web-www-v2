4f9116712b921af90b65eed09851ff9400fbdebe
MagicLegend <MagicLegend@users.noreply.github.com>
2026-01-07 21:14:47 +0100

    854 - Fixed incompabibilities with Sveltia 0.127.0

--- cms/collections.ts
+++ cms/collections.ts
@@ -2,5 +2,6 @@ import { ProjectEnergyCollection } from '../content/energie/collection';
 import { HomeCollection } from '../layouts/collection';
 import { Collection as ArticlesCollection } from '../layouts/newsroom/collection';
 import { Collection as DocumentsCollection } from './documents';
+import { PeopleCollection } from '../layouts/people/collection';
 
-export default [HomeCollection, ArticlesCollection, DocumentsCollection, ProjectEnergyCollection];
+export default [HomeCollection, ArticlesCollection, DocumentsCollection, PeopleCollection, ProjectEnergyCollection];
