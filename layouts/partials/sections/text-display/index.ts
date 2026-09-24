3b8ccae76ec0e911e142db2c378f0d36657d9d43
MagicLegend <MagicLegend@users.noreply.github.com>
2026-09-24 19:53:01 +0200

    Add Text Display Section for temporary Legit SMS notice and update hero section

---
+++ layouts/partials/sections/text-display/index.ts
@@ -0,0 +1,13 @@
+export const TextDisplaySection = {
+  label: 'Text Display Section',
+  required: false,
+  name: 'text-display-section',
+  widget: 'object',
+  fields: [
+    {
+      label: 'Content',
+      name: 'content',
+      widget: 'markdown',
+    },
+  ],
+};
